89e101a200fc1743a90047cd47bb5239802f34f9
Valery Pronin <pownmymail@gmail.com>
2026-02-05 14:22:26 +0300

    refactor: Rename project from 'HappyWheels' to 'Gecko'

--- functions.php
+++ functions.php
@@ -10,26 +10,26 @@ add_theme_support('title-tag');
 
 if (!is_admin()) {
 
-  function happywheels_theme_setup()
+  function gecko_theme_setup()
   {
 
     // Styles
     wp_enqueue_style('swiper-styles', get_theme_file_uri('/assets/css/swiper.min.css'), array(), '10.0.5', 'all');
-    wp_enqueue_style('happywheels-fonts', get_theme_file_uri('/assets/css/fonts.css'), array(), S_VERSION, 'all');
-    wp_enqueue_style('happywheels-styles', get_theme_file_uri('/assets/css/styles.css'), array('happywheels-fonts'), S_VERSION, 'all');
+    wp_enqueue_style('gecko-fonts', get_theme_file_uri('/assets/css/fonts.css'), array(), S_VERSION, 'all');
+    wp_enqueue_style('gecko-styles', get_theme_file_uri('/assets/css/styles.css'), array('gecko-fonts'), S_VERSION, 'all');
 
     // Scripts    
     wp_enqueue_script('swiper-script', get_theme_file_uri('/assets/js/swiper-bundle.min.js'), array(), '10.0.5', true);
-    wp_enqueue_script('happywheels-script', get_theme_file_uri('/assets/js/main.js'), array('swiper-script'), '1.0.0', true);
+    wp_enqueue_script('gecko-script', get_theme_file_uri('/assets/js/main.js'), array('swiper-script'), '1.0.0', true);
   }
-  add_action('after_setup_theme', 'happywheels_theme_setup');
+  add_action('after_setup_theme', 'gecko_theme_setup');
 }
 
-function happywheels_admin_theme_setup()
+function gecko_admin_theme_setup()
 {
   add_theme_support('post-thumbnails');
 }
-add_action('after_setup_theme', 'happywheels_admin_theme_setup');
+add_action('after_setup_theme', 'gecko_admin_theme_setup');
 
 
 function register_custom_post_types()
@@ -38,8 +38,8 @@ function register_custom_post_types()
     'testimonial',
     array(
       'labels'      => array(
-        'name'          => __('Testimonials', 'happywheels'),
-        'singular_name' => __('Testimonial', 'happywheels'),
+        'name'          => __('Testimonials', 'gecko'),
+        'singular_name' => __('Testimonial', 'gecko'),
       ),
       'public'      => true,
       'has_archive' => false,
